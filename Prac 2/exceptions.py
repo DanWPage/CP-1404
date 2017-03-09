b17a142bcb3efec607309a30af886fbbd1ebc9bd
danpage <daniel.page1@my.jcu.edu.au>
2017-03-10 01:12:45 +1000

    Fixed 'if' condition.
    Signed-off-by: danpage <daniel.page1@my.jcu.edu.au>

--- Prac 2/exceptions.py
+++ Prac 2/exceptions.py
@@ -7,9 +7,9 @@ finished = False
 result = 0
 while not finished:
     try:
-        # TODO: this line
-        # TODO: this line
-        pass
-    except :  # TODO - add something after except
+        result = int(input("Enter an integer"))
+        if int(result):
+            finished = True
+    except ValueError:
         print("Please enter a valid integer.")
 print("Valid result is:", result)
