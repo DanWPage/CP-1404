99d06ec3213a49ece0e3b1fbb671a44a0c866c24
danpage <daniel.page1@my.jcu.edu.au>
2017-03-10 14:47:26 +1000

    Finished?

--- Prac 2/word generator.py
+++ Prac 2/word generator.py
@@ -8,12 +8,16 @@ import random
 
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
-word_format = input("Enter the format. e.g.ccvc: ")
+print("Use % for consonents, # for vowels or * for either. You also enter any characters you want.")
+word_format = input("Enter the format: ")
 word = ""
 for kind in word_format.lower():
-    if kind == "c":
+    if kind == "%":
         word += random.choice(CONSONANTS)
-    else:
+    elif kind == "#":
         word += random.choice(VOWELS)
-
+    elif kind == "*":
+        word += random.choice(CONSONANTS + VOWELS)
+    else:
+        word += kind
 print(word)
