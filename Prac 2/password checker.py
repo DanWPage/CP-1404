2b202c04d3da9eb2c5b16241c5b188fdc2a3ae3f
danpage <daniel.page1@my.jcu.edu.au>
2017-03-10 13:41:47 +1000

    Fixed special character conditions.
    Signed-off-by: danpage <daniel.page1@my.jcu.edu.au>

--- Prac 2/password checker.py
+++ Prac 2/password checker.py
@@ -3,9 +3,9 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+MIN_LENGTH = 4
+MAX_LENGTH = 10
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]\<>?{}|"
 
 
@@ -21,25 +21,29 @@ def main():
     while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
-    print("Your " + str(len(password)) + " character password is valid: " + password)
+    print("Your {} character password is valid: {}".format(len(password), password))
 
 
 def is_valid_password(password):
-    # TODO: if length is wrong, return False
-
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
     count_lower = 0
     count_upper = 0
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character
-        pass
-
-    # TODO: if any of the 'normal' counts are zero, return False
-
-    # TODO: if special characters are required, then check the count of those and return False if it's zero
-
-    # if we get here (without having returned False), then the password must be valid
+        if char.islower():
+            count_lower += 1
+        elif char.isupper():
+            count_upper +=1
+        elif char.isnumeric():
+            count_digit += 1
+        elif SPECIAL_CHARS_REQUIRED and char in SPECIAL_CHARACTERS:
+            count_special += 1
+    if count_digit == 0 or count_digit == 0 or count_upper == 0 or count_lower == 0:
+        return False
+    if SPECIAL_CHARS_REQUIRED and count_special == 0:
+        return False
     return True
 
 main()
