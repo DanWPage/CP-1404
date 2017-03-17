41be6dc26da149e909385c1b5982ada48968f323
danpage <daniel.page1@my.jcu.edu.au>
2017-03-17 11:05:19 +1000

    Add user input for the increment. Seperate program into functions.

--- Prac 3/oddName.py
+++ Prac 3/oddName.py
@@ -2,8 +2,33 @@
     Daniel Page.
 """
 
-name = ""
-while not name:
-    name = input("Enter your name :")
-for i in range(0, len(name), 2):
-    print(name[i], end="")
+
+def main():
+    name = get_name()
+    increment = get_increment()
+    print_name(name)
+
+
+def print_name(name):
+    for i in range(0, len(name), 2):
+        print(name[i], end="")
+
+
+def get_increment():
+    invalid = True
+    while invalid:
+        try:
+            number = int(input("Enter the increment: "))
+            return number
+        except ValueError:
+            pass
+
+
+def get_name():
+    name = ""
+    while not name:
+        name = input("Enter your name :")
+    return name
+
+
+main()
