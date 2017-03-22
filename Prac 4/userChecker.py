7fa553aba1ecbc50d413d054a27b009c0bb551c1
danpage <daniel.page1@my.jcu.edu.au>
2017-03-22 14:13:03 +1000

    Search a list for users name.

---
+++ Prac 4/userChecker.py
@@ -0,0 +1,10 @@
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45''BaseInterpreterInterface',
+             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+             'StartServer', 'bob']
+user_name = input('Enter your name: ')
+if user_name in usernames:
+    print('Access Granted')
+else:
+    print("Access Denied")
