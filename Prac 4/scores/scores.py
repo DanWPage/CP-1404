761a4b5e2e4acde8eda1c5ac503ce1d11f5edd47
danpage <daniel.page1@my.jcu.edu.au>
2017-03-24 14:48:13 +1000

    Start on puttting the subjects and scores into a table format.

--- Prac 4/scores/scores.py
+++ Prac 4/scores/scores.py
@@ -15,17 +15,22 @@ def main():
     scores_data = scores_file.readlines()
     subjects = scores_data[0].strip().split(",")
     score_values = []
+    scores_file.close()
     for score_line in scores_data[1:]:
         score_strings = score_line.strip().split(",")
         score_numbers = [int(value) for value in score_strings]
         score_values.append(score_numbers)
-    scores_file.close()
-    for i in range(len(subjects)):
-        print(subjects[i], "Scores:")
-        for score in score_values[i]:
-            print(score)
-        print("Max:", max(score_values[i]))
-        print()
+    print_line(subjects)
+    for i in range(len(score_values)):
+        print_line(score_values[i])
+
+
+def print_line(line):
+    for value in line:
+        print('{:>6}'.format(value), end='')
+    if isinstance(line[0], int):
+        print("\t\tMax: {:2}\t\tMin: {:2}\t\tAverage: {:4}".format(max(line), min(line), sum(line)/len(line)))
+    print()
 
 
 main()
