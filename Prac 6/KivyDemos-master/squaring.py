5cdf0f64d2a727e8b843d4f991b6d852fa5386ff
danpage <daniel.page1@my.jcu.edu.au>
2017-04-14 14:04:03 +1000

    Squaring program now gets input from the .py instead of passing in from .kv.

--- Prac 6/KivyDemos-master/squaring.py
+++ Prac 6/KivyDemos-master/squaring.py
@@ -16,13 +16,14 @@ class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (400, 200)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
-    def handle_calculate(self, value):
+    def handle_calculate(self, ):
         """ handle calculation (could be button press or other call), output result to label widget """
+        value = float(self.root.ids.input_number.text)
         result = value ** 2
         self.root.ids.output_label.text = str(result)
 
