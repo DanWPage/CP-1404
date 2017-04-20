fe350c494c7f6e67b1d0d60e2de8eba4821a42a6
danpage <daniel.page1@my.jcu.edu.au>
2017-04-20 23:28:49 +1000

    Start to prac 7.

---
+++ Prac_7/languages.py
@@ -0,0 +1,5 @@
+from Prac_7 import programminglanguage
+
+ruby = programminglanguage("Ruby", "Dynamic", True, 1995)
+python = programminglanguage("Python", "Dynamic", True, 1991)
+vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
