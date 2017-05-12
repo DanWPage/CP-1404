ff2bf57d2e220b14e6caa2bbc6ace227b6ec6bf0
Bookie <daniel.page1@my.jcu.edu.au>
2017-05-12 12:25:09 +1000

    Add more to prac 8 - taxi sim

--- Prac_8/taxi_sim.py
+++ Prac_8/taxi_sim.py
@@ -1,32 +1,44 @@
 from Prac_8.cars import Taxi
 from Prac_8.cars import SilverServiceTaxi
-from random import randint
 
 TAXIS = [Taxi('Prius', 100),
-         Taxi('Camary', 80),
-         Taxi('Commodore', 100),
-         Taxi('Falcon', 70),
-         Taxi('Elantra', 60),
          SilverServiceTaxi('Hummer', 200, 4),
-         SilverServiceTaxi('Audi A8', 100, 2),
-         SilverServiceTaxi('Merc M8', 120, 3),
-         SilverServiceTaxi('Limo', 100, 2),
-         SilverServiceTaxi('Ferrari F40', 140, 6)]
+         SilverServiceTaxi('Limo', 100, 2)]
 
 
 def main():
+    bill = 0
     print('Lets drive!')
-    choice = menu()
-    while choice != "q":
-        if choice == "c":
+    menu_choice = menu()
+    while menu_choice != "q":
+        if menu_choice == "c":
             show_taxis()
+            taxi = choose_taxi()
+        elif menu_choice == "d":
+            distance = get_distance()
+            taxi.drive(distance)
+            bill += taxi.get_fare()
+        print('Bill to date: ${:.2f}'.format(bill))
+        menu_choice = menu()
+
+
+def choose_taxi():
+    choice = int(input('Choose taxi: '))
+    return TAXIS[choice]
+
+
+def get_distance():
+    return int(input("Drive how far?"))
 
 
 def menu():
     choice = input('q)uit, c)hoose taxi, d)rive')
     return choice
 
+
 def show_taxis():
-    for i in TAXIS:
+    print('Taxis available:')
+    for i in range(len(TAXIS)):
         print(i, '-', TAXIS[i])
-    return
+
+main()
